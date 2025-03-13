f0d06420d70cdc37f4f45798878de0cce9bb619d
Abdou Benbada <gh.abds@gmail.com>
2025-03-13 12:39:46 +0100

    Cleaned up forge config

--- forge.config.ts
+++ forge.config.ts
@@ -10,15 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar : true,
-    // asar: {
-    //   //  unpack: '**/node_modules/better-sqlite3/**/*'
-    //    unpack: 'node_modules/better-sqlite3'
-    // },
-    
     extraResource: ['dist', 'bin'],
-  },
-  rebuildConfig: {
-    onlyModules: ['better-sqlite3'],
   },
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
   plugins: [
